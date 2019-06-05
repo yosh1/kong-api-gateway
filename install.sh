caa0910570c2b0eca6d9fa2a12c654ea335fb38e
Yoshihisa Kaino <yoppy1125yoppy@gmail.com>
2019-06-05 20:28:35 +0900

    Add install shell script

---
+++ install.sh
@@ -0,0 +1,44 @@
+# create kong network
+docker network create kong-net
+
+# just for the case
+sleep 3s
+
+# run postgres
+docker run -d --name kong-database \
+               --network=kong-net \
+               -p 5432:5432 \
+               -e "POSTGRES_USER=kong" \
+               -e "POSTGRES_DB=kong" \
+               postgres:9.6
+
+# wait for postgres container to run
+sleep 5s
+
+# prepare db
+docker run --rm \
+     --network=kong-net \
+     -e "KONG_DATABASE=postgres" \
+     -e "KONG_PG_HOST=kong-database" \
+     -e "KONG_CASSANDRA_CONTACT_POINTS=kong-database" \
+     kong:latest kong migrations up
+
+# wait for postgres to migrate
+sleep 30s
+
+# run kong
+docker run -d --name kong \
+     --network=kong-net \
+     -e "KONG_DATABASE=postgres" \
+     -e "KONG_PG_HOST=kong-database" \
+     -e "KONG_CASSANDRA_CONTACT_POINTS=kong-database" \
+     -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" \
+     -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
+     -e "KONG_PROXY_ERROR_LOG=/dev/stderr" \
+     -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
+     -e "KONG_ADMIN_LISTEN=0.0.0.0:8001, 0.0.0.0:8444 ssl" \
+     -p 8000:8000 \
+     -p 8443:8443 \
+     -p 8001:8001 \
+     -p 8444:8444 \
+     kong:latest
